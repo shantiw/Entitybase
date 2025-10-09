4dfeff3a4975b397e9dd1822b95a465d4b848925
shantiw <shantiw3@outlook.com>
2025-10-09 16:43:33 +0800

    Update NuGet packages

---
+++ Common.JavaScript/JavaScriptValidationAttribute.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shantiw.Data.DataAnnotations
+{
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Property, AllowMultiple = false)]
+    public class JavaScriptValidationAttribute : ValidationAttribute
+    {
+    }
+}
